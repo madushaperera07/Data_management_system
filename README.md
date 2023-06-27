324b13f922077b680a545575f98ab77a5ba01217
madushaperera07 <madushanilakshan9@gmail.com>
2023-06-27 19:21:05 +0530

    create data delete option

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ this is data management system web page create with curd operations.
 now we can use 
 --> Add option
 --> List option
+--> Read option
--- src/Component/Delete.js
+++ src/Component/Delete.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Delete() {
+
+
+
+
+const Delete=()  => {
+
+
+  const [apiId , setapiId] = useState("")
+  const [myData , setMydata] = useState({})
+  const [submit , setSubmit] = useState(false)
+
+  const ReadData=()=>{
+    const apiData = async()=>{
+      const res = await fetch(`http://localhost:9000/saveData/${apiId}`)
+      const data = await res.json()
+
+      if(data){
+        setMydata(data)
+      }
+    }
+  
+    
+  if(apiId.length>0){
+
+    apiData()
+  }
+
+  }
+
+    useEffect(()=>{
+      ReadData()
+       // eslint-disable-next-line
+    },[submit])
+
+
+    const deleteData=(id)=>{
+      fetch(`http://localhost:9000/saveData/${id}`,{
+        method : "DELETE"
+      }).then((data)=>{
+        data.json()
+          ReadData()
+          alert(`Deleted ${id}`)
+      })
+    }
+
+
   return (
     <div>
-      <p>This Is Delete Page</p>
+            <div>
+      <span>Enter Input Id : </span>
+      <input type='text' value={apiId} onChange={(e)=>{
+        e.preventDefault()
+        setapiId(e.target.value)
+      }}/>
+      </div>
+      <div>
+        <button type='submit' onClick={()=>setSubmit((pre)=>pre===false?true:false)}>Submit</button>
+      </div>
+      <div>{
+        myData&&(<div>
+          <h4>{myData.index}</h4>
+          <p>{myData.name}</p>
+          <p>{myData.course}</p>
+          <p>{myData.phone}</p>
+          <p>{myData.city}</p>
+        </div>)
+        }</div>
+
+<div>
+        <button type='delete' onClick={()=>{
+          deleteData(myData.id)
+        }} >Delete</button>
+      </div>
     </div>
   );
 }
