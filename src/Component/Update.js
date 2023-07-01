38c7ca67bf1657934b560c27019c2ca0596de772
madushaperera07 <madushanilakshan9@gmail.com>
2023-07-01 23:24:24 +0530

    create update option

--- src/Component/Update.js
+++ src/Component/Update.js
@@ -1,10 +1,85 @@
 
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Update() {
+
+  const [apiId , setapiId] = useState("")
+  const [myData , setMydata] = useState({
+    index: "",
+    name: "",
+    course: "",
+    phone: "",
+    city: ""
+  })
+  const [submit , setSubmit] = useState(false)
+
+
+
+    useEffect(()=>{
+      axios.get(`http://localhost:9000/saveData/${apiId}`)
+      .then((res)=>{
+        setMydata({ ...myData , index : res.data.index,
+        name: res.data.name,
+      course: res.data.course,
+    phone: res.data.phone,
+      city: res.data.city})
+      }).catch((err)=>{
+        return err
+      })
+    },[submit])
+
+
+  
+
   return (
-    <div>Update</div>
-  )
+    <div>
+      <div>
+      <span>Enter Input Id : </span>
+      <input type='text' value={apiId} onChange={(e)=>{
+        e.preventDefault()
+        setapiId(e.target.value)
+      }}/>
+      </div>
+      <div>
+        <button type='submit' onClick={()=>setSubmit((pre)=>pre===false?true:false)}>Search</button>
+      </div>
+      <div>{
+        myData&&(<div>
+          <div>
+            <label>Index Number</label>
+          <input type='text' value={myData.index}
+          />
+          </div>
+          <div>
+          <label>Names with initials</label>
+          <input type='text' value={myData.name}
+          />
+          </div>
+          <div>
+          <label>Course</label>
+          <input type='text' value={myData.course}
+          />
+          </div>
+          <div>          
+            <label>Phone Number</label>
+          <input type='text' value={myData.phone}
+          />
+          </div>
+          <div>          
+            <label>City</label>
+          <input type='text' value={myData.city}
+          />
+          </div>
+          <div>
+          <button type='submit'>Submit</button>
+          </div>
+
+        </div>)
+        }</div>
+
+    </div>
+  );
 }
 
-export default Update
+export default Update;
